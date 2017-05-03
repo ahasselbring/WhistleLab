cb0f238e9b00b6c3430a95e6e3a21b2b13559e3d
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2017-05-03 17:48:24 +0200

    Simple evaluateOnDatabase implementation

--- Source/Detector/WhistleDetectorInterface.cpp
+++ Source/Detector/WhistleDetectorInterface.cpp
@@ -1,6 +1,14 @@
+#include <iostream>
+
 #include "WhistleDetectorInterface.hpp"
 
 
-void WhistleDetectorInterface::evaluateOnDatabase(const SampleDatabase&)
+void WhistleDetectorInterface::evaluateOnDatabase(const SampleDatabase& db)
 {
+  std::vector<float> samples;
+  float label;
+  unsigned int sampleRate;
+  db.getSample(getPreferredBufferSize(), samples, sampleRate, label);
+  std::cout << "Predicted label: " << classify(samples, sampleRate) << '\n';
+  std::cout << "Actual label: " << label << '\n';
 }
