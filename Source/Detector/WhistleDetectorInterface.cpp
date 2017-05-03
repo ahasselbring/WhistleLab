8a1a99fc07baf7c30edd79022c9979dca5b30de9
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2017-05-03 18:01:10 +0200

    Evaluate on multiple samples

--- Source/Detector/WhistleDetectorInterface.cpp
+++ Source/Detector/WhistleDetectorInterface.cpp
@@ -5,7 +5,12 @@
 
 void WhistleDetectorInterface::evaluateOnDatabase(const SampleDatabase& db)
 {
-  AudioSample as = db.getSample(getPreferredBufferSize());
-  std::cout << "Predicted label: " << classify(as.samples, as.sampleRate) << '\n';
-  std::cout << "Actual label: " << as.label << '\n';
+  AudioSample as;
+  for (unsigned int i = 0; i < 100; i++)
+  {
+    as = db.getSample(getPreferredBufferSize());
+    std::cout << "Sample " << i << ":\n";
+    std::cout << "Predicted label: " << classify(as.samples, as.sampleRate) << '\n';
+    std::cout << "Actual label: " << as.label << '\n';
+  }
 }
