cf71c8342dfbd5bc80636abaf781f96572f87a62
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2017-05-13 18:43:16 +0200

    Better statistics

--- Source/Detector/WhistleDetectorInterface.cpp
+++ Source/Detector/WhistleDetectorInterface.cpp
@@ -15,7 +15,7 @@ void WhistleDetectorInterface::evaluateOnDatabase(const SampleDatabase& db)
   SampleProvider sampleProvider(db);
   AudioSample as;
   unsigned int numberOfTests;
-  unsigned int numberOfCorrectPredictions = 0;
+  unsigned int numberOfPositives = 0, numberOfTruePositives = 0, numberOfTrueNegatives = 0;
   unsigned int offset = 0;
   for (numberOfTests = 0; numberOfTests < maxNumberOfTests; numberOfTests++)
   {
@@ -25,9 +25,19 @@ void WhistleDetectorInterface::evaluateOnDatabase(const SampleDatabase& db)
       break;
     }
     const float predictedLabel = classify(as.samples, as.sampleRate);
-    if ((predictedLabel > 0.5f) == (as.label > 0.5f))
+    const bool whistleDetected = predictedLabel > 0.5f;
+    const bool whistleExpected = as.label > 0.5f;
+    if (whistleExpected)
     {
-      numberOfCorrectPredictions++;
+      numberOfPositives++;
+    }
+    if (whistleDetected && whistleExpected)
+    {
+      numberOfTruePositives++;
+    }
+    else if (!whistleDetected && !whistleExpected)
+    {
+      numberOfTrueNegatives++;
     }
     offset += getPreferredBufferSize() / 2;
   }
@@ -36,5 +46,25 @@ void WhistleDetectorInterface::evaluateOnDatabase(const SampleDatabase& db)
     std::cout << "The sample set seems to be very small!\n";
     return;
   }
-  std::cout << "Accuracy: " << (static_cast<float>(numberOfCorrectPredictions) / static_cast<float>(numberOfTests)) << '\n';
+  const unsigned int numberOfNegatives = numberOfTests - numberOfPositives;
+  const unsigned int numberOfFalseNegatives = numberOfPositives - numberOfTruePositives;
+  const unsigned int numberOfFalsePositives = numberOfNegatives - numberOfTrueNegatives;
+  std::cout << "Sensitivity / Recall / Hit Rate / TPR: "
+    << (static_cast<float>(numberOfTruePositives) / numberOfPositives)
+    << '\n';
+  std::cout << "Specificity / TNR: "
+    << (static_cast<float>(numberOfTrueNegatives) / numberOfNegatives)
+    << '\n';
+  std::cout << "Precision / PPV: "
+    << (static_cast<float>(numberOfTruePositives) / (numberOfTruePositives + numberOfFalsePositives))
+    << '\n';
+  std::cout << "NPV: "
+    << (static_cast<float>(numberOfTrueNegatives) / (numberOfTrueNegatives + numberOfFalseNegatives))
+    << '\n';
+  std::cout << "Accuracy: "
+    << (static_cast<float>(numberOfTruePositives + numberOfTrueNegatives) / numberOfTests)
+    << '\n';
+  std::cout << "Balanced accuracy: "
+    << (0.5f * (static_cast<float>(numberOfTruePositives) / numberOfPositives + static_cast<float>(numberOfTrueNegatives) / numberOfNegatives))
+    << '\n';
 }
