6fccf8eea02111730c668c7ca1b9caac4a92f4a3
Arne Hasselbring <arne.hasselbring@tu-harburg.de>
2017-06-06 21:09:59 +0200

    Add basic audio output

--- Source/Engine/WhistleLabEngine.cpp
+++ Source/Engine/WhistleLabEngine.cpp
@@ -2,6 +2,9 @@
  * @file WhistleLabEngine.cpp implements methods for the whistle lab engine class
  */
 
+#include <QAudioFormat>
+#include <QAudioOutput>
+#include <QIODevice>
 #include <QString>
 
 #include "Detector/WhistleDetectorBase.hpp"
@@ -12,7 +15,9 @@
 
 WhistleLabEngine::WhistleLabEngine(QObject* parent)
   : QObject(parent)
+  , audioDeviceInfo(QAudioDeviceInfo::defaultOutputDevice())
 {
+  audioOutputBuffer.setBuffer(&audioOutputArray);
 }
 
 void WhistleLabEngine::evaluateDetector(const QString& name)
@@ -32,10 +37,99 @@ void WhistleLabEngine::changeDatabase(const QString& readFileName, const QString
   {
     sampleDatabase.writeToFile(writeFileName);
   }
+  selectPlaybackAudioChannel("", 0);
   sampleDatabase.clear();
   if (!readFileName.isEmpty())
   {
     sampleDatabase.readFromFile(readFileName);
   }
   emit sampleDatabaseChanged(sampleDatabase);
+}
+
+void WhistleLabEngine::selectPlaybackAudioChannel(const QString& path, const unsigned int channel)
+{
+  if (!sampleDatabase.exists)
+  {
+    Q_ASSERT(audioOutput == nullptr);
+    return;
+  }
+  if (audioOutput != nullptr)
+  {
+    audioOutput->stop();
+    audioOutputBuffer.close();
+    audioOutputArray.setRawData(nullptr, 0);
+    delete audioOutput;
+    audioOutput = nullptr;
+  }
+
+  if (path.isEmpty())
+  {
+    return;
+  }
+
+  // This method is assumed to be called with valid arguments, thus the asserts.
+  for (auto& audioFile : sampleDatabase.audioFiles)
+  {
+    if (audioFile.path == path)
+    {
+      Q_ASSERT(channel < audioFile.numberOfChannels);
+      for (auto& audioChannel : audioFile.channels)
+      {
+        if (audioChannel.channel == channel)
+        {
+          QAudioFormat format;
+          format.setSampleRate(audioFile.sampleRate);
+          format.setChannelCount(1);
+          format.setSampleSize(8 * sizeof(float));
+          format.setByteOrder(QAudioFormat::LittleEndian);
+          format.setSampleType(QAudioFormat::Float);
+          format.setCodec("audio/pcm");
+          if (!audioDeviceInfo.isFormatSupported(format))
+          {
+            return;
+          }
+
+          audioOutputArray.setRawData(reinterpret_cast<const char*>(audioChannel.samples.data()),
+            audioChannel.samples.size() * sizeof(float));
+          audioOutputBuffer.open(QIODevice::ReadOnly);
+
+          audioOutput = new QAudioOutput(audioDeviceInfo, format, this);
+          return;
+        }
+      }
+      Q_ASSERT(false);
+    }
+  }
+  Q_ASSERT(false);
+}
+
+void WhistleLabEngine::setPlaybackVolume(const qreal volume)
+{
+  if (audioOutput != nullptr)
+  {
+    audioOutput->setVolume(volume);
+  }
+}
+
+void WhistleLabEngine::startPlayback()
+{
+  if (audioOutput != nullptr)
+  {
+    if (audioOutput->state() == QAudio::SuspendedState)
+    {
+      audioOutput->resume();
+    }
+    else
+    {
+      audioOutput->start(&audioOutputBuffer);
+    }
+  }
+}
+
+void WhistleLabEngine::stopPlayback()
+{
+  if (audioOutput != nullptr)
+  {
+    audioOutput->suspend();
+  }
 }
--- Source/Engine/WhistleLabEngine.hpp
+++ Source/Engine/WhistleLabEngine.hpp
@@ -4,11 +4,16 @@
 
 #pragma once
 
+#include <QAudio>
+#include <QAudioDeviceInfo>
+#include <QBuffer>
+#include <QByteArray>
 #include <QObject>
 
 #include "Engine/SampleDatabase.hpp"
 
 
+class QAudioOutput;
 class QString;
 
 /**
@@ -41,7 +46,34 @@ public slots:
    * @param writeFileName the name where the old database is written or an empty string
    */
   void changeDatabase(const QString& readFileName, const QString& writeFileName);
+  /**
+   * @brief selectPlaybackAudioChannel selects an audio channel for playback
+   * @param path the path of the audio file in the sample database
+   * @param channel the channel number of the channel in the file
+   */
+  void selectPlaybackAudioChannel(const QString& path, const unsigned int channel);
+  /**
+   * @brief setPlaybackVolume sets the volume at which channels are played back
+   * @param volume a number between 0 and 1
+   */
+  void setPlaybackVolume(const qreal volume);
+  /**
+   * @brief startPlayback starts or resumes audio playback if there is a channel selected
+   */
+  void startPlayback();
+  /**
+   * @brief stopPlayback pauses audio playback
+   */
+  void stopPlayback();
 private:
+  /// info about the audio playback device
+  QAudioDeviceInfo audioDeviceInfo;
+  /// the audio output
+  QAudioOutput* audioOutput = nullptr;
+  /// the buffer which acts as device for the audio output
+  QBuffer audioOutputBuffer;
+  /// the byte array that points to the playback audio channel
+  QByteArray audioOutputArray;
   /// the open sample database
   SampleDatabase sampleDatabase;
 };
